641cd85f01da06ecf2a47f1a0d657ad8cc3eed02
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-03-08 00:30:45 -0300

    Testing bubblesort

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "bubblesort/Bubblesort.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello World." << endl;
-    return 0;
+
+    int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+    Bubblesort::sort(arr, 10);
+
+    for (int i : arr)
+        cout << i << " ";
+
 }
