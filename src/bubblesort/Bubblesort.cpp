b14a1299b788b6a40baca7a10d25c997b5b64c82
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-03-08 00:30:01 -0300

    Implementing bubblesort algorithm

---
+++ src/bubblesort/Bubblesort.cpp
@@ -0,0 +1,17 @@
+#include "Bubblesort.h"
+
+void Bubblesort::sort(int arr[], int n) {
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+
+        }
+    }
+
+}
---
+++ src/bubblesort/Bubblesort.h
@@ -0,0 +1,13 @@
+#ifndef SORTING_ALGORITHMS_BUBBLESORT_H
+#define SORTING_ALGORITHMS_BUBBLESORT_H
+
+
+class Bubblesort {
+
+public:
+    static void sort(int arr[], int n);
+
+};
+
+
+#endif //SORTING_ALGORITHMS_BUBBLESORT_H
